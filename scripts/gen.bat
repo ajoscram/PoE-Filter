147549da794095f811d5440f14127a4f1e99fb0b
Alejandro Schmidt <ajoscram@gmail.com>
2022-04-19 01:17:41 -0600

    Simplified file generation

--- scripts/gen.bat
+++ scripts/gen.bat
@@ -1,23 +1,25 @@
 @echo off
 setlocal enabledelayedexpansion
 set strict_file_names=Base Lenient Semi-strict Strict Very-strict Uber-strict
-set chaos_types=bodies helmets gloves boots boots nothing
+set chaos_types=Bodies Helmets Gloves Boots Boots Nothing
 set strictness=0
 
 if not exist ".\generated" mkdir ".\generated"
-pfg "src/_main.filter" "generated\Ajo's.filter" .import
-pfg "generated\Ajo's.filter" .econ
+pfg "src/_main.filter" "generated\temp.filter" .import
+pfg "generated\temp.filter" .econ
 
-if not exist ".\generated\Subfilters\" mkdir ".\generated\Subfilters\"
+if not exist ".\generated\chaos" mkdir ".\generated\chaos"
 
 for %%i in (%strict_file_names%) do (
     
-    pfg "generated\Ajo's.filter" "generated\Subfilters\%%i.filter" .strict !strictness!
-    if not exist ".\generated\Subfilters\%%i - chaos" mkdir ".\generated\Subfilters\%%i - chaos"
+    pfg "generated\temp.filter" "generated\%%i.filter" .strict !strictness!
+    if not exist ".\generated\chaos\%%i" mkdir ".\generated\chaos\%%i"
 
     for %%j in (%chaos_types%) do (
-        pfg "generated\Subfilters\%%i.filter" "generated\Subfilters\%%i - chaos\%%j.filter" .tag chaos %%j
+        pfg "generated\%%i.filter" "generated\chaos\%%i\%%j.filter" .tag chaos %%j
     )
 
     set /a strictness+=1
 )
+
+del "generated\temp.filter"
